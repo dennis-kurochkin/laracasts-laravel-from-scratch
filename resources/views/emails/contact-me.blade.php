bffa9efa59096f3ae3a1147e4cf10e5fca9ffd60
Dennis Kurochkin <dennis.kurochkin@gmail.com>
2020-05-24 18:15:36 +0300

    Make custom mail vendors
    Making custom theme for emails

--- resources/views/emails/contact-me.blade.php
+++ resources/views/emails/contact-me.blade.php
@@ -1,13 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>It works again!</h1>
-    <h2>Topic: {{ $topic }}</h2>
-</body>
-</html>
+@component('mail::message')
+
+# Heading
+
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam similique quas id officiis quaerat? Quidem nulla distinctio fugiat! Voluptas accusantium iste magnam molestias consequuntur inventore, quod in nulla? Totam, unde.
+
+* List item 1
+* List item 2
+* List item 3
+
+## Topic: {{ $topic }}
+
+@component('mail::button', ['url' => 'http://yandex.ru'])
+    Visit Yandex
+@endcomponent
+
+@endcomponent
